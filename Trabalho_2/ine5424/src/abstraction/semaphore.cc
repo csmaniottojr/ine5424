20970ce858eaeb56fdc3030e4f0284fbe609e0b1
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2017-06-11 12:49:21 -0300

    Adição da versão base do Trabalho 2, já com os arquivos do Trabalho_1

---
+++ Trabalho_2/ine5424/src/abstraction/semaphore.cc
@@ -0,0 +1,44 @@
+// EPOS Semaphore Abstraction Implementation
+
+#include <semaphore.h>
+
+__BEGIN_SYS
+
+Semaphore::Semaphore(int v): _value(v)
+{
+    db<Synchronizer>(TRC) << "Semaphore(value=" << _value << ") => " << this << endl;
+}
+
+
+Semaphore::~Semaphore()
+{
+    db<Synchronizer>(TRC) << "~Semaphore(this=" << this << ")" << endl;
+}
+
+
+void Semaphore::p()
+{
+    db<Synchronizer>(TRC) << "Semaphore::p(this=" << this << ",value=" << _value << ")" << endl;
+
+    begin_atomic();
+    fdec(_value);
+    if(_value < 0)
+        sleep();
+    else
+        end_atomic();
+}
+
+
+void Semaphore::v()
+{
+    db<Synchronizer>(TRC) << "Semaphore::v(this=" << this << ",value=" << _value << ")" << endl;
+
+    begin_atomic();
+    finc(_value);
+    if(_value < 1)
+        wakeup();
+    else
+        end_atomic();
+}
+
+__END_SYS
