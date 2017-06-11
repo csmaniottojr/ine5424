bb2a7fa69d16f87eaf5c3ba83a9153d7638b1e18
Luis Decker <luisgustavo.decker@gmail.com>
2017-06-11 18:15:01 -0300

    Versão do decker

--- Trabalho_2/ine5424/include/utility/handler.h
+++ Trabalho_2/ine5424/include/utility/handler.h
@@ -6,8 +6,36 @@
 #include <system/config.h>
 
 __BEGIN_UTIL
+class Handler
+{
+public:
+    typedef void (Function) (); //Uma função 
 
-typedef void (Handler)();
+    Handler () { }
+
+    virtual ~Handler () { }
+
+    virtual void operator() () = 0;
+};
+
+class Function_Handler : public Handler
+{
+public:
+
+    Function_Handler (Function * handler) : _handler (handler) { }
+
+    ~Function_Handler (){};
+
+    void operator() () {
+        _handler ();
+    }
+
+
+private:
+    Function * _handler;
+};
+
+
 
 __END_UTIL
 
