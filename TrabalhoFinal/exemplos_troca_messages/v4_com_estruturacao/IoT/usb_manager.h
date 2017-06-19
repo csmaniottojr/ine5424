8de74356b1be2e91ba130ec1c55cf94920c9cc0b
Gilney Nathanael Mathias <gilney_salvo@hotmail.com>
2017-06-18 22:49:43 -0300

    Primeira versão do USBManager, esta com problemas ainda...

--- TrabalhoFinal/exemplos_troca_messages/v4_com_estruturacao/IoT/usb_manager.h
+++ TrabalhoFinal/exemplos_troca_messages/v4_com_estruturacao/IoT/usb_manager.h
@@ -1,15 +1,52 @@
 #ifndef usb_manager_h__
 #define usb_manager_h__
 
+#include <modbus_ascii.h>
+#include <usb.h>
+#include "iot_gateway.h"
+
 using namespace EPOS;
 
 namespace IoT {
-    
+
 class USBManager {
 protected:
-
 public:
+    static int run(IotGateway * _gateway){
+        OStream cout;
+        USB usb;
 
+        char _msg[Modbus_ASCII::MSG_LEN];
+        memset(_msg, '\0', Modbus_ASCII::MSG_LEN);
+        while(true){
+            int len;
+			bool ok_message = false;
+            memset(_msg, '\0', Modbus_ASCII::MSG_LEN);
+			while(!ok_message) {
+				ok_message = true;
+				len = 0;
+				_msg[len++] = usb.get();
+				_msg[len++] = usb.get();
+                cout << "Lido: " << _msg << endl;
+				while(!((_msg[len-2] == '\r') 
+                    && (_msg[len-1] == '\n'))) {
+					_msg[len++] = usb.get();
+					if(len >= Modbus_ASCII::MSG_LEN) {
+						ok_message = false;
+						break;
+					}
+				}
+			}
+
+            if(_gateway != 0){
+                char * msg = new char[len+1];
+                memset(msg, '\0', len+1);
+                memcpy(msg, _msg, len);
+                _gateway->receive(msg, len+1);
+                delete msg;
+            }
+        }
+    }
 };
 
 };
