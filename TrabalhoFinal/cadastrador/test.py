4906ff2c0be19f2b8e7448672f2703047656a636
Cesar Smaniotto Jr <cesarsjb@gmail.com>
2017-06-25 11:24:49 -0300

    corrigido modelo do bd para fazer deleção em cascata

--- TrabalhoFinal/cadastrador/test.py
+++ TrabalhoFinal/cadastrador/test.py
@@ -7,27 +7,27 @@ session = Session()
 
 smart_obj = session.query(SmartObject).filter(
     SmartObject.device_id == 450).first()
-print smart_obj
+print (smart_obj)
 
 smart_obj = session.query(SmartObject).filter(
     SmartObject.device_id == 451).first()
-print smart_obj
-# so = SmartObject('meu objeto fofinho', 450)
-#
-# s1 = Service('servico1')
-# s1.add_parameter(ParameterBoolean('booleano'))
-# s1.add_parameter(ParameterInteger('inteiro',2,5))
-# s2 = Service('servico2')
-# s2.add_parameter(ParameterFloat('pto flutuante', 3.4, 10.6))
-#
-# p_op = ParameterOption('p options')
-# p_op.add_option(Option('op1'))
-# p_op.add_option(Option('op2'))
-# s2.add_parameter(p_op)
-# so.add_service(s1)
-# so.add_service(s2)
-#
-# session.add(so)
-# session.commit()
-#
-# print so
+print (smart_obj)
+so = SmartObject('meu objeto fofinho', 450)
+
+s1 = Service('servico1')
+s1.add_parameter(ParameterBoolean('booleano',5))
+s1.add_parameter(ParameterInteger('inteiro',5,2,5))
+s2 = Service('servico2')
+s2.add_parameter(ParameterFloat('pto flutuante', 5, 3.4, 10.6))
+
+p_op = ParameterOption('p options',5)
+p_op.add_option(Option('op1'))
+p_op.add_option(Option('op2'))
+s2.add_parameter(p_op)
+so.add_service(s1)
+so.add_service(s2)
+
+session.add(so)
+session.commit()
+
+print (so)
